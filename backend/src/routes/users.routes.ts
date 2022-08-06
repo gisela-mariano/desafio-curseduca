e4f34e15b8d583441ddacbf007b6e2a778f9e4f1
Gisela Mariano <gisela-mariano@gotmail.com>
2022-08-06 15:38:27 -0300

    feat: Criação do middleware de verificação de email

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -2,11 +2,12 @@ import { Request, Response, Router } from 'express';
 import createUserController from '../controllers/user/createUser.ctrl';
 import LoginUserController from '../controllers/user/loginUser.ctrl';
 import schemaValidationMiddleware from '../middlewares/schemaValidation.midlw';
+import verifyIfEmailExistsMiddleware from '../middlewares/verifyIfEmailExists.mdlw';
 import userSchema from '../schemas/user.schema';
 
 const userRouter = Router();
 
-userRouter.post('', schemaValidationMiddleware(userSchema), createUserController);
+userRouter.post('', verifyIfEmailExistsMiddleware, schemaValidationMiddleware(userSchema), createUserController);
 userRouter.post('/login', LoginUserController);
 
 export default userRouter;
